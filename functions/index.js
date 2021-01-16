41073ce22aafa7c17bc8fa46efffe85b078d3b8a
Sean Doran <sdoran35@gmail.com>
2021-01-16 14:34:07 -0600

    updates

--- functions/index.js
+++ functions/index.js
@@ -56,7 +56,7 @@ exports.hello = functions.https.onRequest(require('./routes/helloRoutes'));
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
